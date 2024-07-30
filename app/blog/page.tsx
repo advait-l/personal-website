354cbb962966898698d962a49a03c1e30474712e
Advait Lonkar <lonkaradvait@gmail.com>
2024-07-31 01:06:23 +0530

    blog: blog page and posts rendering responsive

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,15 +5,15 @@ import { getSortedPostsData } from "../lib/posts";
 export default function Blog() {
   const allPostsData = getSortedPostsData();
   return (
-    <div className="flex flex-col border w-full h-full p-16">
+    <main className="flex flex-col items-start justify-start w-full h-screen p-8 lg:p-16 bg-gray-100 dark:bg-gray-900">
       <header className="text-4xl font-bold my-8">My Blog</header>
-      <div className="w-full flex flex-col items-center">
-        <ul className="border border-red-500 flex flex-col items-center justify-center w-3/4">
+      <div className="w-full flex flex-col items-center lg:items-start">
+        <ul className="flex flex-col items-center justify-center w-full lg:w-3/4">
           {allPostsData.map(({ id, title, excerpt, date }) => (
             <Link
               href={`/posts/${id}`}
               key={id}
-              className="border border-blue-500 p-4 my-8 w-full flex flex-col items-start justify-between"
+              className="border border-gray-400 dark:border-gray-500 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-800/25 p-4 my-8 w-full flex flex-col items-start justify-between"
             >
               <li>
                 <h1 className="text-xl">{title}</h1>
@@ -25,6 +25,6 @@ export default function Blog() {
           ))}
         </ul>
       </div>
-    </div>
+    </main>
   );
 }
